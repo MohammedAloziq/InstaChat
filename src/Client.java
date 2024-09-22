fa885157505011f496cf5689d58804b0b8a09292
Anonymous <mohammadaloziq8@gmail.com>
2024-09-21 22:24:33 -1100

    Initial Commit

---
+++ src/Client.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+class Client implements Runnable {
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+
+    @Override
+    public void run(){
+        try {
+            // Replace with yours.
+            String ipAddress = "192.168.0.185";
+            client = new Socket(ipAddress, 9999); // Socket client ..
+            out = new PrintWriter(client.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+            InputHandler inHandler = new InputHandler();
+            Thread t = new Thread(inHandler);
+            t.start();
+
+            String inMessage;
+            while((inMessage = in.readLine()) != null) {
+                System.out.println(inMessage);
+            }
+        } catch (IOException e) {
+            shutDown();
+        }
+    }
+
+    public void shutDown() {
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            // ignore
+        }
+    }
+
+    class InputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inReader.readLine();
+                    if (message.equals("/quit")) {
+                        out.println(message);
+                        inReader.close();
+                        shutDown();
+                    } else {
+                        out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                shutDown();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
